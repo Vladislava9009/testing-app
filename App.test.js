81b67e68defaa0b1f9df29393ded03d14c8c92d1
Vladislava <vladislava.bogaturova@gmail.com>
2021-05-24 18:31:36 +0300

    first commit

---
+++ App.test.js
@@ -0,0 +1,9 @@
+import App from './App'
+import {render} from '@testing-library/react-native'
+
+//create an item
+//create multiple items
+//delete an item
+
+//showld show an error whenever I try to create an invalid todo
+//error warning showld disappear ones a valid item is created
